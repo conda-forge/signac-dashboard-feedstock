194d8e0b5cf5bbd504444478937c38f3b96b062d
Bradley Dice <bdice@bradleydice.com>
2018-10-09 21:53:08 -0400

    Updated recipe for 0.1.5, using our tarball instead of Bitbucket's.

--- recipe/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd $SRC_DIR
-git submodule update --init --recursive
-python setup.py install --single-version-externally-managed --record record.txt
--- recipe/run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd $SRC_DIR
-./test.sh
